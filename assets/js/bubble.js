e1ee1da33bbdb26657dcf23522553f08c0bd35cb
AngeKri <10511638@ucn.dk>
2024-10-20 18:50:02 +0200

    animation

---
+++ assets/js/bubble.js
@@ -0,0 +1,10 @@
+// Find alle Bubbles fra index
+const bubbles = document.querySelectorAll('.bubble1, .bubble2, .bubble3, .bubble4');
+
+// Loop gennem hver bubble og giv dem random animations tid og delays
+bubbles.forEach(bubble => {
+    // Random tid mellem 4s og 10s
+    bubble.style.animationDuration = `${Math.random() * 6 + 4}s`;  
+    // Random delay op til 2s
+    bubble.style.animationDelay = `${Math.random() * 2}s`;  
+});
